a14309fad16ccd999806af2560b6504fe2bc8336
Allan Mathenge <allanmathenge67@gmail.com>
2024-12-27 11:50:24 +0300

    allan

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatCardModule} from '@angular/material/card'
+import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [RouterModule, MatCardModule, NgbProgressbar],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
