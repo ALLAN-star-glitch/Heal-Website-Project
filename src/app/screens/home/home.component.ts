f5e0ec87abcf3a8f26b8f13f718e8f1da1602f0a
Allan Mathenge <allanmathenge67@gmail.com>
2024-12-27 20:42:43 +0300

    updated the home page and added modal

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatCardModule} from '@angular/material/card'
-import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, MatCardModule, NgbProgressbar],
+  imports: [RouterModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private modalService: NgbModal) {}
+
+  openModal(content: any) {
+    this.modalService.open(content, { size: 'lg', centered: true });
+  }
 
 }
