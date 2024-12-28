316f1e8da413ec613dbeae5f23ab2571b42434ce
Allan Mathenge <allanmathenge67@gmail.com>
2024-12-28 18:43:01 +0300

    updated the collapsible navbar

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,29 +1,31 @@
 import { Component } from '@angular/core';
-
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';  // Import RouterModule
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
   standalone: true,
   imports: [
-    CommonModule,       // For basic Angular functionality
-    MatToolbarModule,   // For Angular Material Toolbar
-    MatButtonModule,    // For Angular Material Buttons
-    MatIconModule,      // For Angular Material Icons (optional)
-    RouterModule        // Import RouterModule to use routerLink
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule
   ],
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.scss']
 })
 export class MainHeaderComponent {
-  // Any necessary component logic can go here
+  navbarOpen = false;
 
   toggleNavbar() {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    navbarToggler?.classList.toggle('active');
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  closeNavbar() {
+    this.navbarOpen = false;
   }
 }
